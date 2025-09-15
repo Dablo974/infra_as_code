f0c8e6a5668e3251ea4d49358fb4da448d43740e
Gaetan Maronne <123895017+Dablo974@users.noreply.github.com>
2025-09-15 10:49:17 +0000

    retour arrière

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-variable "commit_hash" {
-  description = "Hash du commit Git actuel"
-  type        = string
-}
-
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
@@ -11,23 +6,12 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-11-4-d33s7d9ntdlhbpdsdbeg-4566.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-11-4-d33s7d9ntdlhbpdsdbeg-4566.direct.lab-boris.fr/" //remplacer par url aws
   }
-}
-
-locals {
-  unique_ami = "ami-${substr(var.commit_hash, 0, 8)}"
 }
 
 resource "aws_instance" "demo" {
-  ami           = local.unique_ami
+  ami           = "ami-12345678"
   instance_type = "t2.micro"
-  tags = {
-    Name       = "Instance-GitCommit-${var.commit_hash}"
-    CommitHash = var.commit_hash
-  }
 }
 
-output "instance_id" {
-  value = aws_instance.demo.id
-}
