f7de2a8905bff954b59b17d60be70a666209136f
Gaetan Maronne <123895017+Dablo974@users.noreply.github.com>
2025-09-15 10:50:18 +0000

    gpt

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,16 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-11-4-d33s7d9ntdlhbpdsdbeg-4566.direct.lab-boris.fr/" //remplacer par url aws
+    ec2 = "http://ip10-0-11-4-d33s7d9ntdlhbpdsdbeg-4566.direct.lab-boris.fr/"
   }
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
+# Génère un suffixe aléatoire à chaque "terraform apply"
+resource "random_id" "ami_suffix" {
+  byte_length = 2
 }
 
+resource "aws_instance" "demo" {
+  ami           = "ami-${random_id.ami_suffix.hex}"
+  instance_type = "t2.micro"
+}
